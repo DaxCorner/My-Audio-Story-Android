f6a616e735503f1d80ba334bc3b461eb335c9f55
sourav <khungersourav30@gmail.com>
2019-05-13 19:55:00 +0530

    Init commit

--- app/src/main/java/com/doozycod/childrenaudiobook/ListenAudioStory.java
+++ app/src/main/java/com/doozycod/childrenaudiobook/ListenAudioStory.java
@@ -4,20 +4,35 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.IOException;
+
 import static com.doozycod.childrenaudiobook.R.drawable.pop_up_bg;
 
 public class ListenAudioStory extends AppCompatActivity {
-    ImageView recordAudioButton, home_btn_listen_audio, library_btn_listen, login_btn_listen, popup_login, popup_signup, login_dialog,listen_book,use_bg_music;
+    ImageView recordAudioButton, home_btn_listen_audio, library_btn_listen, login_btn_listen, popup_login, popup_signup, login_dialog, listen_book, use_bg_music;
     Dialog myDialog;
-
+    String AudioSavePathInDevice;
+    MediaRecorder mediaRecorder;
+    MediaPlayer mediaPlayer ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        if (!Permissions.Check_RECORD_AUDIO(this) && !Permissions.Check_STORAGE(this)) {
+            Permissions.Request_RECORD_AUDIO(this, 10);
+            Permissions.Request_STORAGE(this, 10);
+
+        }
+
         setContentView(R.layout.activity_listen_audio_story);
         recordAudioButton = findViewById(R.id.record_audio);
         login_btn_listen = findViewById(R.id.login_btn_listen);
@@ -25,6 +40,8 @@ public class ListenAudioStory extends AppCompatActivity {
         use_bg_music = findViewById(R.id.use_background_audio);
         home_btn_listen_audio = findViewById(R.id.home_btn_listen_audio);
         library_btn_listen = findViewById(R.id.lib_btn_listen_audio);
+
+
         recordAudioButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -90,6 +107,32 @@ public class ListenAudioStory extends AppCompatActivity {
         myDialog.show();
     }
 
+    public void recordAudio() {
+        AudioSavePathInDevice =
+                Environment.getExternalStorageDirectory().getAbsolutePath() + "/" +
+                        "" + "AudioRecording.3gp";
+
+        MediaRecorderReady();
+
+        try {
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+    public void MediaRecorderReady(){
+        mediaRecorder=new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        mediaRecorder.setOutputFile(AudioSavePathInDevice);
+    }
     public void showLoginPopUp(View v) {
 
         myDialog.setContentView(R.layout.custom_login_popup);
